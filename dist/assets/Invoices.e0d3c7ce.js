1bde6da9487a9a1582a3e5638091331d81a1ab1c
manoweb <iveta@ma-no.org>
2023-01-05 09:44:11 +0100

    first

---
+++ dist/assets/Invoices.e0d3c7ce.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o,b as s}from"./index.ab4634f1.js";const n={},t=s("h1",null,"Invoices",-1),_=[t];function r(a,i){return c(),o("div",null,_)}const d=e(n,[["render",r]]);export{d as default};
---
+++ dist/assets/NotFound.666bf9d2.js
@@ -0,0 +1,1 @@
+import{_ as n,r as a,o as c,c as s,b as o,a as e,d as r,w as _}from"./index.ab4634f1.js";const d={},l=o("h1",null,"Not Found",-1);function u(i,f){const t=a("RouterLink");return c(),s("div",null,[l,o("p",null,[e("OOOPS! Go "),r(t,{to:"/"},{default:_(()=>[e("home")]),_:1})])])}const p=n(d,[["render",u]]);export{p as default};
